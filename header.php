e44a2efa7f1e2a38675a1254464761447db3b959
saipunya <sumetchoorat@gmail.com>
2025-11-07 11:23:22 +0700

    Update header.php

--- header.php
+++ header.php
@@ -19,6 +19,16 @@ require_once 'functions.php';
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: "Sarabun", sans-serif;
+                font-weight: 400;
+                font-style: normal;
+            }
+        </style>
     </head>
 
     <body class="bg-light">
