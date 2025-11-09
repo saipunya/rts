5041e840dbce3404ee739636a943935285d11254
saipunya <sumetchoorat@gmail.com>
2025-11-09 07:01:20 +0700

    update connect

--- functions.php
+++ functions.php
@@ -1,11 +1,18 @@
 <?php
 $host = 'localhost';
 $username = 'rts_user';
-$password = 'sumetchoorat4631022';
+$password = 'sumet4631022';
 $database = 'rts_db';
 $mydb = mysqli_connect($host, $username, $password, $database);
 if (!$mydb) {
     die("Connection failed: " . mysqli_connect_error());
 }
 $mysqli = new mysqli($host, $username, $password, $database);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+} else {
+    echo "Connected successfully";
+}
+
+$mysqli->close();
 ?>
