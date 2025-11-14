81843e6bc3495acd2fa5440cbfbde4e62bae2424
saipunya <sumetchoorat@gmail.com>
2025-11-14 10:47:24 +0700

    update

--- member_form.php
+++ member_form.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'functions.php';
-require_admin();
+require_login();
 include 'header.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'create';
--- members.php
+++ members.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'functions.php';
-require_admin();
+require_login();
 include 'header.php';
 
 // fetch messages
